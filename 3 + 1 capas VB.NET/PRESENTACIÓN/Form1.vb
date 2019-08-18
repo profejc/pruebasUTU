9bb2dc266e9c430009c7d7f843d692923cfd0840
profejc <54222346+profejc@users.noreply.github.com>
2019-08-18 20:34:32 -0300

    tercer commit
    variables nuevas

--- 3 + 1 capas VB.NET/PRESENTACIÓN/Form1.vb
+++ 3 + 1 capas VB.NET/PRESENTACIÓN/Form1.vb
@@ -1,5 +1,10 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MsgBox("HOLA GIT!!")
+        Dim numerito As Integer = 12
+        Dim otroNumerito As Integer = 15
+        MsgBox(numerito + otroNumerito)
+
+
     End Sub
 End Class
